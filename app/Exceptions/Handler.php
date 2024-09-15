1fff26053ffa4d2bd831186ad812d722fe181333
Shibbir Ahmad <shibbirahmad920229@gmail.com>
2024-09-15 12:58:52 +0600

    feat :star: upgraded to laravel 7

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace App\Exceptions;
-
-use Exception;
+use Throwable;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
+
+
+   
     /**
      * A list of the exception types that are not reported.
      *
@@ -32,9 +34,14 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return void
      */
-    public function report(Exception $exception)
+    public function report(Throwable $exception)
     {
         parent::report($exception);
+    }
+
+
+    public function shouldReport(Throwable $exception){
+         
     }
 
     /**
@@ -44,8 +51,16 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
     }
+    
+
+    public function renderForConsole($output, Throwable $exception){
+
+    }
+
+
+
 }
