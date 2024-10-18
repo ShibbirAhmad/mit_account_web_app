35ee962120dd858e865b7f1994d16fec782fdc6b
Saiful Islam <104412703+devsaiful77@users.noreply.github.com>
2024-10-18 21:23:42 +0600

    delete junk file

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -7,7 +7,8 @@
     ),
     'aliases' => 
     array (
-      'PDF' => 'Barryvdh\\DomPDF\\Facade',
+      'Pdf' => 'Barryvdh\\DomPDF\\Facade\\Pdf',
+      'PDF' => 'Barryvdh\\DomPDF\\Facade\\Pdf',
     ),
   ),
   'beyondcode/laravel-dump-server' => 
